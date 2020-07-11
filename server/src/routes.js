292b0a242d8e8d2eb3fc9f09963e85d6bb98c8d0
Daniel Panzarini <panzarinidaniel@outlook.com>
2020-07-10 23:24:39 -0300

    Começando as funcionalidades avançadas

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
 
 const SessionController = require('./controllers/SessionController');
 const OngsController = require('./controllers/OngsController');
@@ -10,11 +11,38 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.store);
 
 routes.get('/ongs', OngsController.index);
-routes.post('/ongs', OngsController.store);
 
-routes.get('/incidents', IncidentsController.index);
-routes.post('/incidents', IncidentsController.store);
-routes.delete('/incidents/:id', IncidentsController.delete);
+// validando o BODY
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.number().required().min(10).max(11),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2)
+  })
+}), OngsController.store);
+
+// validadando o HEADER
+routes.get('/incidents', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required()
+  }).unknown()
+}), IncidentsController.index);
+
+// validando query
+routes.post('/incidents', celebrate({
+  [Segments.QUERY]: Joi.object().keys({
+    page: Joi.number()
+  })
+}), IncidentsController.store);
+
+// validando o PARAMS
+routes.delete('/incidents/:id', celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.number().required()
+  })
+}), IncidentsController.delete);
 
 routes.get('/profile', ProfileController.index);
 
